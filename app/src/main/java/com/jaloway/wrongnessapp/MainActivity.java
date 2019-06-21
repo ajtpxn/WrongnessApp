e3c94a27c340c59a304a4588ce5f8aaf3985fae3
ajtpxn <ajtpxn@gmail.com>
2019-06-21 15:58:43 -0600

    first commit

---
+++ app/src/main/java/com/jaloway/wrongnessapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.jaloway.wrongnessapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void showWrong(View view) {
+        ImageView image = (ImageView) findViewById(R.id.imageView);
+        image.setImageResource(R.drawable.wrong);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
